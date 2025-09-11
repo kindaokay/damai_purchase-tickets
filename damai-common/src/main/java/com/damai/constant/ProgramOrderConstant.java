40302eaf20bcf1cfc38fa89b1c70385e22bc3a72
陈路林 <501412612@qq.com>
2025-09-11 22:57:48 +0800

    修改延迟取消时间

--- damai-common/src/main/java/com/damai/constant/ProgramOrderConstant.java
+++ damai-common/src/main/java/com/damai/constant/ProgramOrderConstant.java
@@ -11,7 +11,7 @@ public class ProgramOrderConstant {
     
     public static final String DELAY_ORDER_CANCEL_TOPIC ="d_delay_order_cancel_topic";
     
-    public static final Long DELAY_ORDER_CANCEL_TIME = 1L;
+    public static final Long DELAY_ORDER_CANCEL_TIME = 2L;
     
     public static final TimeUnit DELAY_ORDER_CANCEL_TIME_UNIT = TimeUnit.MINUTES;
     
