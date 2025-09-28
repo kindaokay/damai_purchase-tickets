e0cb19ab41bfb58b4e28d817ce1e16ceca26cb15
陈路林 <501412612@qq.com>
2025-09-28 09:56:12 +0800

    修正退款逻辑

--- damai-server/damai-pay-service/src/main/java/com/damai/service/PayService.java
+++ damai-server/damai-pay-service/src/main/java/com/damai/service/PayService.java
@@ -187,6 +187,7 @@ public class PayService {
         return tradeCheckVo;
     }
     
+    @Transactional(rollbackFor = Exception.class)
     public String refund(RefundDto refundDto) {
         PayBill payBill = payBillMapper.selectOne(Wrappers.lambdaQuery(PayBill.class)
                 .eq(PayBill::getOutOrderNo, refundDto.getOrderNumber()));
@@ -206,12 +207,16 @@ public class PayService {
         RefundResult refundResult = 
                 payStrategyHandler.refund(refundDto.getOrderNumber(), refundDto.getAmount(), refundDto.getReason());
         if (refundResult.isSuccess()) {
+            PayBill updatePayBill = new PayBill();
+            updatePayBill.setId(payBill.getId());
+            updatePayBill.setPayBillStatus(PayBillStatus.REFUND.getCode());
+            payBillMapper.updateById(updatePayBill);
             RefundBill refundBill = new RefundBill();
             refundBill.setId(uidGenerator.getUid());
             refundBill.setOutOrderNo(payBill.getOutOrderNo());
             refundBill.setPayBillId(payBill.getId());
             refundBill.setRefundAmount(refundDto.getAmount());
-            refundBill.setRefundStatus(1);
+            refundBill.setRefundStatus(2);
             refundBill.setRefundTime(DateUtils.now());
             refundBill.setReason(refundDto.getReason());
             refundBillMapper.insert(refundBill);
