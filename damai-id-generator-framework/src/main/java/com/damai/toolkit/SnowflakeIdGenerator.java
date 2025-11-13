ac506f892c6cb319e00c1de45112c9efde294611
陈路林 <501412612@qq.com>
2025-11-13 09:57:15 +0800

    修复id生成的说明

--- damai-id-generator-framework/src/main/java/com/damai/toolkit/SnowflakeIdGenerator.java
+++ damai-id-generator-framework/src/main/java/com/damai/toolkit/SnowflakeIdGenerator.java
@@ -137,7 +137,7 @@ public class SnowflakeIdGenerator {
                 timestamp = tilNextMillis(lastTimestamp);
             }
         } else {
-            // 不同毫秒内，序列号置为 1 - 2 随机数
+            // 不同毫秒内，序列号置为 1 - 3 随机数
             sequence = ThreadLocalRandom.current().nextLong(1, 3);
         }
         
